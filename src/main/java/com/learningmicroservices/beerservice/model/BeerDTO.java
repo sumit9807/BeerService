66f2fb33ed0ec99bbce71671304814ab1984c13d
SUMIT SINGH <singhsumit28lko@gmail.com>
2022-03-10 22:54:16 +0530

    Adding Been Validation. Closes #8

--- src/main/java/com/learningmicroservices/beerservice/model/BeerDTO.java
+++ src/main/java/com/learningmicroservices/beerservice/model/BeerDTO.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -14,14 +18,32 @@ import java.util.UUID;
 @NoArgsConstructor
 @Builder
 public class BeerDTO {
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
+
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
     private String beerName;
+
+    @NotNull
     private BeerStyleEnum beerStyle;
-    private String upc;
+
+    @Positive
+    @NotNull
+    private Long upc;
+
+    @NotNull
+    @Positive
     private BigDecimal price;
+
     private Integer quanityOnHand;
 
 }
